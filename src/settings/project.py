bb2f65eb57594f79834738da727fe3c614688d2e
Kelson da Costa Medeiros <kelsoncm@gmail.com>
2026-06-12 10:58:04 -0300

    del(settings): remover variável PROJECT_LAST_STARTUP por não ser mais necessária

--- src/settings/project.py
+++ src/settings/project.py
@@ -1,5 +1,3 @@
-import datetime
-
 from sc4py.env import env, env_as_bool
 
 SHOW_SUPPORT_FORM = env_as_bool("SHOW_SUPPORT_FORM", True)
@@ -9,7 +7,6 @@ PROJECT_COMPANY = env("PROJECT_COMPANY", "IFRN")
 PROJECT_TITLE = env("PROJECT_TITLE", "Integrador AVA")
 PROJECT_SUBTITLE = env("PROJECT_SUBTITLE", "Sistema de integração de ambientes virtuais de aprendizagem")
 PROJECT_VERSION = env("PROJECT_VERSION", "1.1.061")
-PROJECT_LAST_STARTUP = int(datetime.datetime.timestamp(datetime.datetime.now()) * 1000)
 PROJECT_COPYRIGHT = env("PROJECT_COPYRIGHT", "🄯2025 IFRN")
 PROJECT_LICENSE = env("PROJECT_LICENSE", "Licença MIT")
 PROJECT_LICENSE_URL = env("PROJECT_LICENSE_URL", "https://opensource.org/license/mit")
